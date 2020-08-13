73a70ca3a627a4c2faf6c235493f3ceec3fac4a5
jinwan <jinwan@tencent.com>
2020-08-13 17:37:50 +0800

    someconsole delete

--- demo7/lib/download.js
+++ demo7/lib/download.js
@@ -3,9 +3,6 @@ const fs = require('fs')
 
 module.exports = async ({ song, res, findName }) => {
   const data = await requireMp3(res.url)
-  fs.writeFileSync('文件.txt', 'Node.js 中文网', 'utf8', () => {})
-
-  fs.writeFileSync('demo1.txt', '111', 'utf8', () => {})
   fs.writeFileSync(`${findName}.mp3`, data, 'binary', function (err) {
     if (err) {
       console.log('shibai')
